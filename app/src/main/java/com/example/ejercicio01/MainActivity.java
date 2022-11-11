dd4156deea4a0986de4720d2944364b167885daf
andreasweet <andreascam1718@gmail.com>
2022-11-11 12:21:07 +0100

    jj

--- app/src/main/java/com/example/ejercicio01/MainActivity.java
+++ app/src/main/java/com/example/ejercicio01/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -17,10 +18,16 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    // ------Buena Praxis----
     private final int NUMINICIALCIGARROS = 20;
+    private final int COSTEPAQUETE = 5;
+
+    //Contadores
     private int numCigarrillos;
     private int dineroAhorrado;
-    NumberFormat nf = NumberFormat.getCurrencyInstance();
+    private NumberFormat nf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+        nf = NumberFormat.getCurrencyInstance();
 
         numCigarrillos = NUMINICIALCIGARROS;
         binding.contentMain.lblNumCigarrosMain.setText("Cigarros restantes: "+ numCigarrillos);
@@ -38,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 numCigarrillos -= 1;
-                if(numCigarrillos <1){
+                if(numCigarrillos  == 0){
                     numCigarrillos = NUMINICIALCIGARROS;
                     dineroAhorrado += 5;
                     Toast.makeText(MainActivity.this, "MUY BIEN, TE HAS ACABADO LA CAJA, AHORRAS "+nf.format(5), Toast.LENGTH_SHORT).show();
@@ -47,19 +55,22 @@ public class MainActivity extends AppCompatActivity {
                 binding.contentMain.lblNumCigarrosMain.setText("Cigarros restantes: "+ numCigarrillos);
             }
         });
-
-
-
-
-
-
-
-
-
-
-
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("DINERO", dineroAhorrado);
+        outState.putInt("CIGARROS", numCigarrillos);
+    }
 
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        dineroAhorrado = savedInstanceState.getInt("DINERO");
+        numCigarrillos = savedInstanceState.getInt("CIGARROS");
 
+        binding.contentMain.lblDineroAhorradoMain.setText("Dinero Ahorrado: "+nf.format(dineroAhorrado));
+        binding.contentMain.lblNumCigarrosMain.setText("Cigarros restantes: "+ numCigarrillos);
+    }
 }
